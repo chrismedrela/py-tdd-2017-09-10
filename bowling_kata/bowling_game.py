113ce7f7c11080195dc8f86aea95c59d9f790f56
Chris Medrela <chris.medrela+github@gmail.com>
2017-09-12 11:12:28 +0200

    .

--- bowling_kata/bowling_game.py
+++ bowling_kata/bowling_game.py
@@ -1,4 +1,7 @@
 class BowlingGame:
+    FRAMES_IN_GAME = 10
+    NUMBER_OF_PINS = 10
+
     def __init__(self):
         self._rolls = []
 
@@ -6,4 +9,31 @@ class BowlingGame:
         self._rolls.append(pins)
 
     def score(self):
-        return sum(self._rolls)
+        frame_index = 0
+        total = 0
+        for _ in range(self.FRAMES_IN_GAME):
+            first_roll = self._rolls[frame_index]
+
+            is_strike = first_roll == self.NUMBER_OF_PINS
+
+            bonus = 0
+            if is_strike:
+                frame_sum = first_roll
+                bonus = self._rolls[frame_index+1] + self._rolls[frame_index+2]
+
+            else:
+                second_roll = self._rolls[frame_index+1]
+                frame_sum = first_roll + second_roll
+                
+                is_spare = frame_sum == self.NUMBER_OF_PINS
+                if is_spare:
+                    bonus = self._rolls[frame_index+2]
+
+            total += frame_sum + bonus
+
+            if is_strike:
+                frame_index += 1
+            else:
+                frame_index += 2
+
+        return total
--- bowling_kata/bowling_game_tests.py
+++ bowling_kata/bowling_game_tests.py
@@ -4,17 +4,42 @@ from bowling_game import BowlingGame
 
 
 class BowlingGameTests(unittest.TestCase):
+    def setUp(self):
+        self.game = BowlingGame()
+
+    def roll_many(self, pins, rolls):
+        for i in range(rolls):
+            self.game.roll(pins)
+
     def test_when_all_throws_one_then_score_should_be_twenty(self):
-        game = BowlingGame()
-        for i in range(20):
-            game.roll(1)
-        self.assertEqual(game.score(), 20)
+        self.roll_many(pins=1, rolls=20)
+        self.assertEqual(self.game.score(), 20)
 
     def test_when_all_throws_in_gutter_then_score_should_be_zero(self):
-        game = BowlingGame()
-        for i in range(20):
-            game.roll(0)
-        self.assertEqual(game.score(), 0)
+        self.roll_many(pins=0, rolls=20)
+        self.assertEqual(self.game.score(), 0)
+
+    def test_when_one_spare_then_next_roll_is_counted_twice(self):
+        self.roll_many(pins=5, rolls=2)
+        self.game.roll(4)
+        self.roll_many(pins=0, rolls=17)
+        self.assertEqual(self.game.score(), 18)
+
+    def test_when_spare_in_last_frame_then_third_roll_is_counted_once(self):
+        self.roll_many(pins=0, rolls=18)
+        self.roll_many(pins=5, rolls=3)
+        self.assertEqual(self.game.score(), 15)
+
+    def test_when_strike_then_next_two_rolls_are_counted_twice(self):
+        self.game.roll(10)
+        self.roll_many(pins=4, rolls=18)
+        bonus = 2*4
+        self.assertEqual(self.game.score(), 10 + 18*4 + bonus)
+
+    def test_when_perfect_game_then_score_should_be_300(self):
+        self.roll_many(pins=10, rolls=12)
+        self.assertEqual(self.game.score(), 300)
+
 
 
 if __name__ == '__main__':
